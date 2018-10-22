7242bb49cbee105d70b6daafe1896b51512989e5
Masonfrith <gedsaro@gmail.com>
2018-10-21 18:59:51 -0600

    Add project files.

---
+++ cmpt436_a1_cSharp_Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cmpt436_a1_cSharp_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
