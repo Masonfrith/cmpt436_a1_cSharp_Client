7f3e3b6a4a58a1f2ab038ba2bad55d5c5436980f
Masonfrith <gedsaro@gmail.com>
2018-10-24 16:39:58 -0600

    ChatClient...
    add small hardcode interaction with server to test connecting accross language types.

--- cmpt436_a1_cSharp_Client/ChatClient.cs
+++ cmpt436_a1_cSharp_Client/ChatClient.cs
@@ -10,19 +10,35 @@ namespace cmpt436_a1_cSharp_Client
     {
         TcpClient clientConnection;
         String userInput;
+        String fromServer;
         Stream stream;
         StreamReader sin;
         StreamWriter sout;
+        
 
 
         public ChatClient(String ip, int portNumber)
         {
+            this.fromServer = "";
+            this.userInput = "";
+
             Console.WriteLine("Client attempting to connect to " + ip + " on port # " + portNumber);
             this.clientConnection = new TcpClient(ip, portNumber);
             this.stream = clientConnection.GetStream();
             this.sin = new StreamReader(this.stream);
             this.sout = new StreamWriter(this.stream);
             Console.WriteLine("Now connected to the server!");
+
+            //lets test a first automatic communication without user input.
+
+            this.sout.WriteLine("Hi I am from cSharp! wut up?");
+            this.fromServer = this.sin.ReadLine();
+            Console.WriteLine("Server: " + this.fromServer);
+            this.sout.WriteLine("hah");
+            this.fromServer = this.sin.ReadLine();
+            Console.WriteLine("Server: "_ + this.fromServer);
+            this.sout.WriteLine("QUIT");
+            this.fromServer = this.sin.ReadLine();
         }
 
     }
