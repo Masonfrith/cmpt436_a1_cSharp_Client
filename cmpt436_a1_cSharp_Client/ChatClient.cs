62fe86f0153aab8c956496f5bb324bc9a209b029
Masonfrith <gedsaro@gmail.com>
2018-10-24 16:34:17 -0600

    create class CharClient, create class Program...
    Program, the main program loop of the chat client system.
    will later take arguments on startup if given to set the ip and port number to attempt connection to.
    uses defaults if no args given.
    now creates a client and attempts to connect to server.
    
    ChatClient, handles the client, creates a Tcp client which attempts to connect to server at given ip and port.
    Sets up socket stream reader and writer for communication.

---
+++ cmpt436_a1_cSharp_Client/ChatClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace cmpt436_a1_cSharp_Client
+{
+    class ChatClient
+    {
+        TcpClient clientConnection;
+        String userInput;
+        Stream stream;
+        StreamReader sin;
+        StreamWriter sout;
+
+
+        public ChatClient(String ip, int portNumber)
+        {
+            Console.WriteLine("Client attempting to connect to " + ip + " on port # " + portNumber);
+            this.clientConnection = new TcpClient(ip, portNumber);
+            this.stream = clientConnection.GetStream();
+            this.sin = new StreamReader(this.stream);
+            this.sout = new StreamWriter(this.stream);
+            Console.WriteLine("Now connected to the server!");
+        }
+
+    }
+}
--- cmpt436_a1_cSharp_Client/Program.cs
+++ cmpt436_a1_cSharp_Client/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Net.Sockets;
 
 namespace cmpt436_a1_cSharp_Client
 {
     class Program
     {
+
+        static ChatClient chatClient;
+        static String defaultIP = "localhost";
+        static int defaultPort = 40010;
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); // test local repository commiting
+
+            Console.WriteLine("Starting up chat program...");
+            chatClient = new ChatClient(defaultIP, defaultPort);
+
+            //interaction loop will go here!
+
+            Console.WriteLine("Exiting Chat program.");
         }
     }
 }
