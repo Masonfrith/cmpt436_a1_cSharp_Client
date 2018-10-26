b3f4ba8123ca59bfa85a8b1ae29e245504b900cb
Masonfrith <gedsaro@gmail.com>
2018-10-26 09:48:19 -0600

    add comments, move autochatting to own method...
    add 0 to 10s delay between sending of each auto message...
    
    add shutdown method to client, closes input/output streams and socket.

--- cmpt436_a1_cSharp_Client/ChatClient.cs
+++ cmpt436_a1_cSharp_Client/ChatClient.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace cmpt436_a1_cSharp_Client
 {
@@ -29,16 +30,44 @@ namespace cmpt436_a1_cSharp_Client
             this.sout = new StreamWriter(this.stream);
             Console.WriteLine("Now connected to the server!");
 
+            this.sout.AutoFlush = true; // seems to be needed to make it work, not sure why, needs reading
+
             //lets test a first automatic communication without user input.
-            this.sout.AutoFlush = true;
+            this.autoChat();
+        }
+
+        private void autoChat()
+        {
+            Console.WriteLine("Starting autochat...");
+
+            Random randomWait = new Random();
+
+            Thread.Sleep(randomWait.Next(0, 10000)); // pause for 0-10s
+            Console.WriteLine("Me: Hi I am from cSharp! wut up?");
             this.sout.WriteLine("Hi I am from cSharp! wut up?");
             this.fromServer = this.sin.ReadLine();
             Console.WriteLine("Server: " + this.fromServer);
+            Thread.Sleep(randomWait.Next(0, 10000)); // pause for 0-10s
+            Console.WriteLine("Me: hah");
             this.sout.WriteLine("hah");
             this.fromServer = this.sin.ReadLine();
             Console.WriteLine("Server: " + this.fromServer);
+            Thread.Sleep(randomWait.Next(0, 10000)); // pause for 0-10s
+            Console.WriteLine("Me: QUIT");
             this.sout.WriteLine("QUIT");
             this.fromServer = this.sin.ReadLine();
+            Console.WriteLine("Server: " + this.fromServer);
+
+            
+        }
+
+        internal void shutDownClient()
+        {
+            this.sin.Close();
+            this.sout.Close();
+            this.stream.Close();
+            this.clientConnection.Close();
+            Console.WriteLine("Connections closed, ready to shutdown.");
         }
 
     }
